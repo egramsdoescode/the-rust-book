53be5b900c6f03524ea79a07cb79c4300bff5c6a
ethandoes-code <ethandoescode@gmail.com>
2024-04-02 11:53:07 -0500

    Finish control flow

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -19,4 +19,65 @@ fn main() {
             break;
         }
     }
+
+    counter = 0;
+
+    // Returning values from the loops can be done using break as well
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is: {result}"); // Result is 20
+    
+    // Loop labels
+    counter = 0;
+
+    'counting_up: loop {
+        println!("Count = {counter}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if counter == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        counter += 1;
+    }
+    println!("End count = {counter}");
+
+    // While loops
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!!");
+
+    // For loops
+
+    // Loopling through a collection:
+
+    let a = [10, 20, 30, 40 ,50];
+    
+    for element in a {
+        println!("The value is: {element}");
+    }
+
+    // Counting a range
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
 }
